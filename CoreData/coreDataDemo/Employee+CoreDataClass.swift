81614d8e2f7e17e4cc0fed90157ab9598da4306c
codecat15 <codecat15@gmail.com>
2020-06-13 20:10:12 -0500

    Introduction to core data

---
+++ CoreData/coreDataDemo/Employee+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Employee+CoreDataClass.swift
+//  coreDataDemo
+//
+//  Created by CodeCat15 on 6/12/20.
+//  Copyright © 2020 CodeCat15. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Employee)
+public class Employee: NSManagedObject {
+
+}
