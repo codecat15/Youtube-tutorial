329254dd326eb41137a552334d01e3ef591dc9fe
Codecat15 <codecat15@gmail.com>
2022-07-26 17:17:31 -0500

    copy on write demo

---
+++ CopyOnWrite/Demo/Model/Car.swift
@@ -0,0 +1,16 @@
+//
+//  Solution.swift
+//  Demo
+//
+//  Created by Codecat15 on 7/20/22.
+//
+
+import Foundation
+
+
+class Car {
+    var name: String
+    init(withName _name: String) {
+        name = _name
+    }
+}
