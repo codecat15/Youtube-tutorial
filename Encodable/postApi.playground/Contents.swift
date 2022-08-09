a70f70c107a731e07fb3a67da3ca83c5940a2ecc
Codecat15 <codecat15@gmail.com>
2022-08-09 18:50:28 -0500

    Updated request contract
    Updated the request contract since they changed in the API

--- Encodable/postApi.playground/Contents.swift
+++ Encodable/postApi.playground/Contents.swift
@@ -40,13 +40,17 @@ struct Endpoint
 
 struct UserRegistrationRequest : Encodable
 {
-    let FirstName, LastName, Email, Password : String
+    let Name, Email, Password : String
+    /*
+     //Note: When I made the demo this was the initial contract used, after that the API has undergone some updates and the request contract has changed, I have kept the old contract just for reference purpose.
+     
+     let FirstName, LastName, Email, Password : String
 
     enum CodingKeys: String, CodingKey {
         case FirstName = "First_Name"
         case LastName = "Last_Name"
         case Email, Password
-    }
+    }*/
 }
 
 struct UserRegistrationResponse: Decodable {
@@ -66,24 +70,22 @@ struct User
         //code to register user
         var urlRequest = URLRequest(url: URL(string: Endpoint.registerUser)!)
         urlRequest.httpMethod = "post"
-
-        let request = UserRegistrationRequest(FirstName: "ravi", LastName: "codecat15", Email: "codecat15@gmail.com", Password: "1234")
-
+        let request = UserRegistrationRequest(Name: "ravi", Email: "codecat15@gmail.com", Password: "1234")
+        
         do {
-
             let requestBody = try JSONEncoder().encode(request)
             urlRequest.httpBody = requestBody
             urlRequest.addValue("application/json", forHTTPHeaderField: "content-type")
-
         } catch let error {
             debugPrint(error.localizedDescription)
         }
 
         URLSession.shared.dataTask(with: urlRequest) { (data, httpUrlResponse, error) in
-
-            if(data != nil && data?.count != 0)
-            {
+            if(data != nil && data?.count != 0) {
                 do {
+                    // uncomment this code to view the server response in console
+//                    let str = String(decoding: data!, as: UTF8.self)
+//                    debugPrint(str)
                     let response = try JSONDecoder().decode(UserRegistrationResponse.self, from: data!)
                     debugPrint(response.data.name)
                 }
@@ -103,7 +105,6 @@ struct User
 
         do {
             let requestBody = try JSONSerialization.data(withJSONObject: dataDictionary, options: .prettyPrinted)
-
             urlRequest.httpBody = requestBody
             urlRequest.addValue("application/json", forHTTPHeaderField: "content-type")
 
@@ -112,9 +113,7 @@ struct User
         }
 
         URLSession.shared.dataTask(with: urlRequest) { (data, httpUrlResponse, error) in
-
-            if(data != nil && data?.count != 0)
-            {
+            if(data != nil && data?.count != 0) {
                 let response = String(data: data!, encoding: .utf8)
                 debugPrint(response!)
             }
